e498458bafbcd8e30ba00b6a2afaf6f10bf2ed5a
prasadchintaman <73985730+prasadchintaman@users.noreply.github.com>
2026-03-13 15:22:17 +0530

    Enhance Docker build function with credentials
    Refactor buildDocker function to include Docker login and push steps.

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,3 +1,23 @@
 def call(String imageName) {
-    sh "docker build -t ${imageName} ."
+
+    script {
+
+        withCredentials([usernamePassword(
+            credentialsId: 'docker-id',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )]) {
+
+            sh """
+            docker build -t \$DOCKER_USER/${imageName}:latest .
+
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+
+            docker push \$DOCKER_USER/${imageName}:latest
+            """
+
+        }
+
+    }
+
 }
