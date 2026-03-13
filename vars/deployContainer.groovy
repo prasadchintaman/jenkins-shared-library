0f904ca236c5f00beeecf3d2bcb554892a9d7391
prasadchintaman <73985730+prasadchintaman@users.noreply.github.com>
2026-03-13 15:50:19 +0530

    Update Docker run command to use specific image

--- vars/deployContainer.groovy
+++ vars/deployContainer.groovy
@@ -3,7 +3,6 @@ def call(String containerName, String imageName) {
     sh """
     docker stop ${containerName} || true
     docker rm ${containerName} || true
-    docker run -d -p 5000:5000 --name ${containerName} ${imageName}
+    docker run -d -p 5000:5000 --name ${containerName} prasad1993/${imageName}
     """
-
 }
