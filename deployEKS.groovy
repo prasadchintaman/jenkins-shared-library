bc9fd3c8432ef5862313d86df433d6ffb52bdd80
prasadchintaman <73985730+prasadchintaman@users.noreply.github.com>
2026-03-22 22:36:04 +0530

    Remove deployment function from deployEKS.groovy

--- deployEKS.groovy
+++ deployEKS.groovy
@@ -1,10 +1,1 @@
-def call(clusterName, region, imageName) {
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-creds']]) {
-        sh """
-        aws eks update-kubeconfig --region ${region} --name ${clusterName}
-        kubectl set image deployment/ecommerce ecommerce=${imageName} --record || true
-        kubectl apply -f deployment.yaml
-        kubectl apply -f service.yaml
-        """
-    }
-}
+
